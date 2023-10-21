648011a85dbee2f0b06142b60d258ebaf816d1cd
Myo Myat Aung <myatmyo299@gmail.com>
2023-10-22 02:31:19 +0630

    UPdate

--- app/final/page.jsx
+++ app/final/page.jsx
@@ -5,14 +5,32 @@ import React from "react";
 
 const Final = () => {
   return (
-    <main className="app p-20">
-      <Image
-        src="/assets/images/awba.svg"
-        alt="logo"
-        width={300}
-        height={200}
-        className="object-contain"
-      />
+    <main className="app">
+      <Nav centerIcon={true} />
+      <section className="w-full flex-center flex-col mt-6">
+        <p className="desc text-center">
+          <span className="font-bold text-2xl text-white">ပိုးသတ်‌ဆေး</span>{" "}
+          နှင့်{" "}
+          <span className="font-bold text-2xl text-white">
+            ရွက်ဖျန်းအားဆေး သိန်း၇၅၀ဖိုး
+          </span>{" "}
+          ဝယ်ယူသူတိုင်းအတွက်
+        </p>
+        <h1 className="font-bold text-4xl mt-4 text-center">
+          <span className="text-white text-center">ဗလာမပါ</span>
+        </h1>
+        <h1 className="font-bold text-4xl mt-4 mb-4 text-center">
+          <span className="text-white">ရွှေကံစမ်းမဲအစီအစဥ်</span>
+          {/* <span className="text-gold">ရွှေကံစမ်းမဲအစီအစဥ်</span> */}
+        </h1>
+        <section className="feed">
+          <div className="relative w-full flex-center">
+            <h1 className="prompt_card text-3xl text-white fond-semibold">
+              Thank you!
+            </h1>
+          </div>
+        </section>
+      </section>
     </main>
   );
 };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Link from "next/link"; 
+import Link from "next/link";
 
-const Nav = () => {
+const Nav = ({ centerIcon = false }) => {
   return (
-    <nav className="flex-between w-full mb-4 mt-4 pt-3">
+    <nav className={`${centerIcon? "flex-center" : "flex-between"} w-full mb-4 mt-4 pt-3`}>
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/awba-logo.svg"
@@ -13,11 +13,11 @@ const Nav = () => {
           className="object-contain"
         />
       </Link>
-      <div className="flex">
-        {/* <button type="button" className="mt-5 w-full black_btn">
+      {/* <div className="flex">
+        <button type="button" className="mt-5 w-full black_btn">
           Export CSV
-        </button> */}
-      </div>
+        </button>
+      </div> */}
     </nav>
   );
 };
